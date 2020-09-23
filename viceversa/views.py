7eb5009453caf79711c0849cffe476ec0801a35b
gergvit <mailvitalij@mail.ru>
2020-09-24 00:26:55 +0300

    viceversa-2

--- viceversa/views.py
+++ viceversa/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
+
+def reverse(request):
+	user_text = request.GET['message']
+	reversed_text = user_text[::-1]
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversed':reversed_text})
