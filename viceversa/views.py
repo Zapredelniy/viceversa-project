b62ebe8f0a01003a85e313df578432a66aedb6e7
gergvit <mailvitalij@mail.ru>
2020-09-24 23:19:52 +0300

    Third

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from collections import Counter
 
 def home(request):
 	return render(request, 'home.html')
@@ -6,4 +7,5 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['message']
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversed':reversed_text})
+	number_words = len(user_text.split())
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversed':reversed_text, 'number_words':number_words})
